f1fefc18864d51e1a214970ac12e845d423c0d5c
João Roberto Silva Felizardo <futurepromise12@gmail.com>
2024-12-12 15:49:37 -0300

    getting a user by Id

--- src/main/java/com/devSilva/workshopmongo/services/UserService.java
+++ src/main/java/com/devSilva/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.devSilva.workshopmongo.services;
 
 import com.devSilva.workshopmongo.domain.User;
 import com.devSilva.workshopmongo.repository.UserRepository;
+import com.devSilva.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = repo.findById(id);
+
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
