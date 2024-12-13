3beb5a2862126f8c7a46ffc33b75144d6bf79611
João Roberto Silva Felizardo <futurepromise12@gmail.com>
2024-12-13 14:43:42 -0300

    Creating Entity Post with nested user

---
+++ src/main/java/com/devSilva/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.devSilva.workshopmongo.repository;
+
+import com.devSilva.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post,String> {
+}
